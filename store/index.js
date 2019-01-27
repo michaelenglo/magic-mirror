26c0bc073d6bb547d5eb42a606b8d27da82d946d
Nico Alimin <nico@hackcapital.com>
2019-01-27 11:40:38 -0800

    Add current user reducer

---
+++ store/index.js
@@ -0,0 +1,24 @@
+import { configureStore, getDefaultMiddleware } from 'redux-starter-kit';
+import { currentUserReducer, defaultUser } from './currentUserReducer';
+import { friendsReducer, defaultFriends } from './friendsReducer';
+
+const middleware = [...getDefaultMiddleware()];
+
+const reducer = {
+  currentUser: currentUserReducer,
+  friends: friendsReducer,
+};
+
+const preloadedState = {
+  currentUser: defaultUser,
+  friends: defaultFriends,
+}
+
+const store = configureStore({
+  reducer,
+  middleware,
+  preloadedState,
+  devTools: process.env.NODE_ENV !== 'production',
+})
+
+export default store;
